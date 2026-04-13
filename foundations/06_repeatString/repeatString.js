f001e3a23268abd0fae65b68dfe815a8114d02a5
John C <johnacapuano@gmail.com>
2026-04-13 07:28:29 -0400

    submitting solutions

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = "";
+    if (num >= 0) {
+        for (let i = 0; i < num;) {
+            newString += string;
+            i++;
+        }
+        
+        } else {
+        let errorString = "ERROR";
+        return errorString;
+    }
+    return newString;
 };
 
 // Do not edit below this line
