988550b38aee977ad79d17ec338e4076b6617662
John C <johnacapuano@gmail.com>
2026-04-14 08:51:36 -0400

    finished reverse string excercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseArray = [];
+    let newStringArray = string.split("");
+    console.log(newStringArray);
+    const arrayLength = newStringArray.length
+    for (i=0; i < arrayLength; i++) {
+         reverseArray += newStringArray.pop();
+          //console.log(reverseArray);
+          //console.log(newStringArray);
+      } 
+    //console.log(reverseArray);
+    return reverseArray.toString();
 };
 
 // Do not edit below this line
